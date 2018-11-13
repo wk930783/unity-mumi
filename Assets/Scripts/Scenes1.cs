01f239bed68e19403f24d38de1987b91e0aba273
yuki <wk93078@gmail.com>
2018-11-13 20:16:27 +0800

    unity hajime

---
+++ Assets/Scripts/Scenes1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scenes1 : MonoBehaviour {
+    // Use this for initialization
+    public Button BtnStart;
+    public Button BtnExit;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        BtnStart.onClick.AddListener(BtnStartOnClick);
+        BtnExit.onClick.AddListener(BtnExitOnClick);
+    }
+
+    public void BtnStartOnClick()
+    {
+        Debug.Log("yee");
+        Application.LoadLevel("Scenes2");
+    }
+
+    public void BtnExitOnClick()
+    {
+        Debug.Log("wryyyyyyyy");
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        
+    }
+}
